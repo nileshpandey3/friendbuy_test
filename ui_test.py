6516e784e3eb9b7854e36f4b6389fdd97969f970
Nilesh Pandey <nileshpandey3@gmail.com>
2021-05-15 16:54:55 -0700

    update ui tests

--- ui_test.py
+++ ui_test.py
@@ -3,8 +3,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+
+"""Test to verify a user sharing flow"""
+
 driver = webdriver.Chrome('./chromedriver')
-driver.fullscreen_window()
+# include user creds to get past authentication pop up
 driver.get("https://fbt-tester:HdXWyVowcUFrcGaMjsofGGJ4@webstore.fbot-sandbox.me")
 get_ten_dollars = driver.find_element_by_css_selector("[id*=fbt-ribbon-]")
 driver.execute_script("arguments[0].click();", WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[id*=fbt-ribbon-]"))))
@@ -17,7 +20,7 @@ friends_email_input.send_keys("anothertest@gmail.com")
 send_email = driver.find_element_by_id("c5").click()
 thanks_message = driver.find_element_by_id("bM")
 get_thanks_text = thanks_message.text
-print(get_thanks_text)
-assert get_thanks_text == "Thanks for sharing TurboTax"
+assert get_thanks_text == "Thanks for sharing TurboTax", "Thanks/Success message doesn't appear"
 driver.close()
+driver.quit()
 
